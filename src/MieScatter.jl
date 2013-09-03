f85a2225e87cfe98cd6884c42fd3432257c2c12b
Olli Wilkman <olli.wilkman@iki.fi>
2013-09-04 00:36:48 +0300

    Enable freely determined angles

--- src/MieScatter.jl
+++ src/MieScatter.jl
@@ -6,18 +6,32 @@ export compute_mie, size_parameter
 size_parameter(radius::Real, wavelength::Real) = 2pi * radius / wavelength
 
 function compute_mie(size_param::Real, ref_idx::Number, N_angles::Integer)
+    delta_angle = pi / (N_angles-1)
+    compute_mie(size_param, ref_idx, [(i-1)*delta_angle for i = 1:N_angles])
+end
+
+function compute_mie(size_param::Real, ref_idx::Number, angles::Vector{Float64})
     y = size_param * ref_idx
     x_stop = size_param + 4 * size_param^0.3333 + 2.0
 
     y_modulus = abs(y)
     n_max = int(max(x_stop, y_modulus) + 15)
-    delta_angle = pi / 2 / (N_angles-1)
     
-    mu_table = [cos((j-1)*delta_angle) for j = 1:N_angles]
+    mu_table = [cos(theta) for theta in angles]
+    N_angles = length(angles)
+    idx_forward_scatter = -1
+    idx_backscatter = -1
+    for i = 1:N_angles
+        if abs(angles[i]-0.0) < 1e-10
+            idx_forward_scatter = i
+        end
+        if abs(angles[i]-pi) < 1e-10
+            idx_backscatter = i
+        end
+    end
     
     d = zeros(Complex128, n_max+1)
     
-    # TODO: verify this loop
     for n = n_max-1:-1:1
         rn = n+1
         d[n] = (rn/y) - (1 / (d[n+1] + rn/y))
@@ -27,8 +41,8 @@ function compute_mie(size_param::Real, ref_idx::Number, N_angles::Integer)
     pi1 = ones(Float64, N_angles)
     piX = zeros(Float64, N_angles)
     
-    s1 = zeros(Complex128, 2*N_angles-1)
-    s2 = zeros(Complex128, 2*N_angles-1)
+    s1 = zeros(Complex128, N_angles)
+    s2 = zeros(Complex128, N_angles)
     tau = zeros(Float64, N_angles)
     
     psi0 = cos(size_param)
@@ -36,7 +50,7 @@ function compute_mie(size_param::Real, ref_idx::Number, N_angles::Integer)
     chi0 = -sin(size_param)
     chi1 = cos(size_param)
     
-    S = zeros(Float64, 2*N_angles-1, 4)
+    S = zeros(Float64, N_angles, 4)
     
     xi1 = Complex128(psi1, -chi1)
     Qsca = 0.0
@@ -54,16 +68,11 @@ function compute_mie(size_param::Real, ref_idx::Number, N_angles::Integer)
         
         for j = 1:N_angles
             piX[j] = pi1[j]
-            jj = 2*N_angles - j
             tau[j] = n * mu_table[j] * piX[j] - (n+1)*pi0[j]
             t = (-1)^n
             p = (-1)^(n-1)
             s1[j] = s1[j] + fn * (an*piX[j] + bn*tau[j])
             s2[j] = s2[j] + fn * (an*tau[j] + bn*piX[j])
-            if j != jj
-                s1[jj] = s1[jj] + fn * (an*piX[j]*p + bn*tau[j]*t)
-                s2[jj] = s2[jj] + fn * (an*tau[j]*t + bn*piX[j]*p)
-            end
         end
         
         psi0 = psi1
@@ -79,12 +88,12 @@ function compute_mie(size_param::Real, ref_idx::Number, N_angles::Integer)
     end
     
     Qsca *= 2/size_param^2
-    Qext = (4 / size_param^2) * real(s1[1])
-    Qback = (4 / size_param^2) * abs(s1[end])^2
+    Qext = (idx_forward_scatter > 0) ? (4 / size_param^2) * real(s1[1]) : NaN
+    Qback = (idx_backscatter > 0) ? (4 / size_param^2) * abs(s1[end])^2 : NaN
     
-    for i = 1:2*N_angles-1
+    for i = 1:N_angles
         S[i,1] = 0.5 * (abs(s1[i])^2 + abs(s2[i])^2)
-        S[i,2] = -0.5 * (abs(s1[i])^2 + abs(s2[i])^2)
+        S[i,2] = -0.5 * (abs(s1[i])^2 - abs(s2[i])^2)
         S[i,3] = real(s2[i] * conj(s1[i]))
         S[i,4] = imag(s2[i] * conj(s1[i]))
     end
