844bcbcbc8525df1485287befa1ab433c2284657
Olli Wilkman <olli.wilkman@iki.fi>
2018-11-22 11:20:38 +0200

    Add some more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,39 +4,52 @@ using Test
 
 @testset "Cross sections" begin
 
-# These tests are made compared to the FORTRAN77 Mie code
-# written by Karri Muinonen to the precision of the output
-# of that code.
-
-# Test computation of scattering, extinction and absorbsion
-# cross sections.
-
-# Size parameter 2, refractive index 1.33
-S, Qsca, Qext, Qback = compute_mie(2.0, 1.33, 181)
-Qabs = Qext-Qsca
-@test isapprox(Qsca, 0.71295 ; atol = 6)
-@test isapprox(Qext, 0.71295 ; atol = 6)
-@test isapprox(Qabs, 0.0 ; atol = 10)
-
-# Size parameter 2, refractive index 1.6
-S, Qsca, Qext, Qback = compute_mie(2.0, 1.6, 181)
-Qabs = Qext-Qsca
-@test isapprox(Qsca, 2.4834 ; atol = 5)
-@test isapprox(Qext, 2.4834 ; atol = 5)
-@test isapprox(Qabs, 0.0 ; atol = 10)
-
-# Size parameter 20, refractive index 1.33
-S, Qsca, Qext, Qback = compute_mie(20.0, 1.33, 181)
-Qabs = Qext-Qsca
-@test isapprox(Qsca, 0.21401E+01 ; atol = 6)
-@test isapprox(Qext, 0.21401E+01 ; atol = 6)
-@test isapprox(Qabs, 0.66613E-14 ; atol = 6)
-
-# Size parameter 20, refractive index 1.6
-S, Qsca, Qext, Qback = compute_mie(20.0, 1.6, 181)
-Qabs = Qext-Qsca
-@test isapprox(Qsca, 0.26115E+01 ; atol = 6)
-@test isapprox(Qext, 0.26115E+01 ; atol = 6)
-@test isapprox(Qabs, 0.39968E-14 ; atol = 6)
+    # These tests are made compared to the FORTRAN77 Mie code
+    # written by Karri Muinonen to the precision of the output
+    # of that code.
+    
+    # Test computation of scattering, extinction and absorbsion
+    # cross sections.
+    
+    # Size parameter 2, refractive index 1.33
+    S, Qsca, Qext, Qback = compute_mie(2.0, 1.33, 181)
+    Qabs = Qext-Qsca
+    @test isapprox(Qsca, 0.71295 ; atol = 6)
+    @test isapprox(Qext, 0.71295 ; atol = 6)
+    @test isapprox(Qabs, 0.0 ; atol = 10)
+    
+    # Size parameter 2, refractive index 1.6
+    S, Qsca, Qext, Qback = compute_mie(2.0, 1.6, 181)
+    Qabs = Qext-Qsca
+    @test isapprox(Qsca, 2.4834 ; atol = 5)
+    @test isapprox(Qext, 2.4834 ; atol = 5)
+    @test isapprox(Qabs, 0.0 ; atol = 10)
+    
+    # Size parameter 20, refractive index 1.33
+    S, Qsca, Qext, Qback = compute_mie(20.0, 1.33, 181)
+    Qabs = Qext-Qsca
+    @test isapprox(Qsca, 0.21401E+01 ; atol = 6)
+    @test isapprox(Qext, 0.21401E+01 ; atol = 6)
+    @test isapprox(Qabs, 0.66613E-14 ; atol = 6)
+    
+    # Size parameter 20, refractive index 1.6
+    S, Qsca, Qext, Qback = compute_mie(20.0, 1.6, 181)
+    Qabs = Qext-Qsca
+    @test isapprox(Qsca, 0.26115E+01 ; atol = 6)
+    @test isapprox(Qext, 0.26115E+01 ; atol = 6)
+    @test isapprox(Qabs, 0.39968E-14 ; atol = 6)
 
 end # cross section testset
+
+
+@testset "Return types and NaN" begin
+    S, Qsca, Qext, Qback = compute_mie(20.0, 1.6, [pi/2])
+    @test typeof(S) == Array{Float64,2}
+    @test size(S) == (1,4)
+    @test typeof(Qsca) == Float64
+    @test typeof(Qext) == Float64
+    @test typeof(Qback) == Float64
+    @test isnan(Qext)
+    @test isnan(Qback)
+    @test !isnan(Qsca)
+end
